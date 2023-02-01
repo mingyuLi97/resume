47b60a388ddc6bc584597dc7bc20b1a5ee6dd4f8
limy <mingyuli1997@gmail.com>
2023-02-01 22:22:25 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    App
+    <MainEditor />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MainEditor from "./components/editor/MainEditor.vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/editor/MainEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="editor-wrapper">
+    <EditorMenu :editor="(editor as Editor)"></EditorMenu>
+    <EditorContent class="editor" :editor="editor" />
+    <div v-html="html"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useEditor, EditorContent, Editor } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+import { ref } from "vue";
+import EditorMenu from "./EditorMenu.vue";
+
+const html = ref("");
+
+const editor = useEditor({
+  content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
+  extensions: [StarterKit],
+  onUpdate({ editor }) {
+    html.value = editor.getHTML();
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.editor-wrapper {
+  width: 500px;
+  min-height: 50px;
+  background-color: #ddd;
+  border-radius: 10px;
+}
+
+.editor {
+  background-color: red;
+  ol,
+  li,
+  ul {
+    list-style: initial;
+  }
+}
+</style>
